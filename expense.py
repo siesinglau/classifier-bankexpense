53a5f9997922b1996e27efdacb18f344cf6897ff
siesinglau <46617383+siesinglau@users.noreply.github.com>
2019-01-13 17:49:30 -0800

    Update expense.py

--- expense.py
+++ expense.py
@@ -4,6 +4,9 @@
 ### Once trained with sufficient data, the neural network model will be accurate enough to be used for prediction.
 ### The prediction is done by supplying a CSV file named predict.csv.
 ###
+### Author: Sie Sing Lau
+### Date: 13 Jan 2019
+###
 ### Limitations:
 ### 1. The model is currently limited to be able to map to 20 expense categories
 ### 2. Only the first 8 words of the expense text description is used. The rest of the text is ignored.
